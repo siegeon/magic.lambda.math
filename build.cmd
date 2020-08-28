5082d6de99cee2bbd35c72b7e3f8041c4e04ce7e
Thomas Hansen <thomas@gaiasoul.com>
2020-08-28 11:06:20 +0300

    Updating version, getting ready for release

--- build.cmd
+++
@@ -1,7 +0,0 @@
-
-set version=%1
-set key=%2
-
-cd %~dp0
-dotnet build magic.lambda.math/magic.lambda.math.csproj --configuration Release --source https://api.nuget.org/v3/index.json
-dotnet nuget push magic.lambda.math/bin/Release/magic.lambda.math.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
